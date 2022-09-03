9fa4eaecd3d5d7ba18b85ca23f6fcb896c139917
Matteo DeSimone <thematteod@gmail.com>
2022-09-03 13:12:09 -0400

    Initial commit

---
+++ StaticExerciseProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExerciseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+        }
+    }
+}
---
+++ StaticExerciseProject/TempConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExerciseProject
+{
+    internal class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f-32)*(5/9);
+        }
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return (c * (9/5)) + 32;
+        }
+    }
+}
